41758f4431dd86f6b06f44468a7c9c6189f493d4
Hanz Badua <hanzb@keemail.me>
2022-06-27 19:53:27 -0500

    version 0.1

---
+++ CraftMagicMirrors.cs
@@ -0,0 +1,24 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CraftMagicMirrors
+{
+	public class CraftMagicMirrors : Mod
+	{
+        public override void AddRecipes()
+        {
+            _ = Recipe.Create(ItemID.MagicMirror, 1)
+                .AddRecipeGroup("IronBar", 4)
+                .AddIngredient(ItemID.Glass, 6)
+                .AddIngredient(ItemID.RecallPotion, 10)
+                .AddTile(TileID.Anvils)
+                .Register();
+
+            _ = Recipe.Create(ItemID.IceMirror, 1)
+                .AddIngredient(ItemID.MagicMirror, 1)
+                .AddIngredient(ItemID.IceBlock, 10)
+                .Register();
+        }
+    }
+}
